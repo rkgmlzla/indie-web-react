b7baab6dbbb2a8cfee8fd7d7e84a611b90d2b233
imsuajeon <suajeon@naver.com>
2025-07-26 23:51:30 +0900

    아 맞다 지도

--- src/pages/map/MapPage.jsx
+++ src/pages/map/MapPage.jsx
@@ -17,24 +17,19 @@ const PageWrapper = styled.div`
   height: 100vh;
   display: flex;
   flex-direction: column;
-  overflow: hidden; 
-`;
-
-const FixedHeaderArea = styled.div`
-  flex-shrink: 0;
-  position: relative;
+  overflow: hidden;
 `;
 
 const MapPage = () => {
   return (
     <PageWrapper>
-      <FixedHeaderArea>
-        <Header title="가까운 공연 찾기" />
-        <MapView />
-        <MapTime />
-        <Divider />
-      </FixedHeaderArea>
-      
+      <Header title="가까운 공연 찾기" initialSearchTab="공연/공연장" />
+      <div style={{ height: '30px' }} />
+
+      <MapView />
+      <MapTime />
+      <Divider />
+
       <ScrollableContainer>
         <MapGrid data={sampleData} />
       </ScrollableContainer>
@@ -45,17 +40,76 @@ const MapPage = () => {
 export default MapPage;
 
 const sampleData = [
-  { id: 1, poster: samplePosterImage1, title: 'A Place Called Sound', venue: '홍대 언플러그드emememememememem', address: '서울특별시 마포구 와우산로 33', time: '오후 5시' },
-  { id: 2, poster: samplePosterImage2, title: '아직 흐르고 있어', venue: '뭥미', address: '인천시 연수구', time: '오후 11시' },
-  { id: 3, poster: samplePosterImage3, title: 'FRIDAY THE 13TH', venue: '여기가어디냐', address: '서울특별시 마포구 와우산로 19999999999999999999999999999999999', time: '오후 3시 30분' },
-  { id: 4, poster: samplePosterImage1, title: '아직 자고 있어', venue: '홍대 언플러그드emememememememem', address: '서울특별시 마포구 와우산로 33', time: '오후 5시' },
-  { id: 5, poster: samplePosterImage2, title: '아직 먹고 있어', venue: '어디더라아ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ', address: '서울특별시 마포구 와우산로 33', time: '오후 11시' },
-  { id: 6, poster: samplePosterImage3, title: '으어엉어어어어어엄청 긴 이르으으으으으으으으으으으으으으으으으으으으음', venue: '몰라유', address: '서울특별시 마포구 와우산로 33', time: '오후 3시 30분' },
-  { id: 7, poster: samplePosterImage1, title: '아직 공부하고 있어', venue: '홍대 언플러그드emememememememem', address: '서울특별시 인천대로', time: '오후 5시' },
-  { id: 8, poster: samplePosterImage2, title: '아직 뭐하고 있어', venue: '몰라', address: '서울특별시 마포구 와우산로 33', time: '오후 11시' },
+  {
+    id: 1,
+    poster: samplePosterImage1,
+    title: 'A Place Called Sound',
+    venue: '홍대 언플러그드emememememememem',
+    address: '서울특별시 마포구 와우산로 33',
+    time: '오후 5시',
+  },
+  {
+    id: 2,
+    poster: samplePosterImage2,
+    title: '아직 흐르고 있어',
+    venue: '뭥미',
+    address: '인천시 연수구',
+    time: '오후 11시',
+  },
+  {
+    id: 3,
+    poster: samplePosterImage3,
+    title: 'FRIDAY THE 13TH',
+    venue: '여기가어디냐',
+    address: '서울특별시 마포구 와우산로 19999999999999999999999999999999999',
+    time: '오후 3시 30분',
+  },
+  {
+    id: 4,
+    poster: samplePosterImage1,
+    title: '아직 자고 있어',
+    venue: '홍대 언플러그드emememememememem',
+    address: '서울특별시 마포구 와우산로 33',
+    time: '오후 5시',
+  },
+  {
+    id: 5,
+    poster: samplePosterImage2,
+    title: '아직 먹고 있어',
+    venue:
+      '어디더라아ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ',
+    address: '서울특별시 마포구 와우산로 33',
+    time: '오후 11시',
+  },
+  {
+    id: 6,
+    poster: samplePosterImage3,
+    title:
+      '으어엉어어어어어엄청 긴 이르으으으으으으으으으으으으으으으으으으으으음',
+    venue: '몰라유',
+    address: '서울특별시 마포구 와우산로 33',
+    time: '오후 3시 30분',
+  },
+  {
+    id: 7,
+    poster: samplePosterImage1,
+    title: '아직 공부하고 있어',
+    venue: '홍대 언플러그드emememememememem',
+    address: '서울특별시 인천대로',
+    time: '오후 5시',
+  },
+  {
+    id: 8,
+    poster: samplePosterImage2,
+    title: '아직 뭐하고 있어',
+    venue: '몰라',
+    address: '서울특별시 마포구 와우산로 33',
+    time: '오후 11시',
+  },
 ];
 
-const sampleData2 = { // <MapWideCard data={sampleData2} />
+const sampleData2 = {
+  // <MapWideCard data={sampleData2} />
   title: '홍대 언플러그드',
   time: '오후 5시',
   name: '홍대 언플러그드',
